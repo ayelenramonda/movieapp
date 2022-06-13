c56d22856806f0cd42dfcf4aeadb481ab812c99f
AyeRamonda <aramonda@telered.net.ar>
2022-06-13 09:22:53 -0300

    cambios

--- js/segunda entrega/js/prestamo.js
+++
@@ -1,9 +0,0 @@
-class Prestamo {
-    constructor(nombre, taza, cuotas, limite, tipodeprestamo) {
-      this.nombre = nombre;
-      this.taza = taza;
-      this.cuotas = cuotas;
-      this.limite = limite;
-      this.tipodeprestamo = tipodeprestamo;
-    }
-}
